98c968e49ec9e78782ccd3292ae0a06b084d1b36
lyf <2598895690.com>
2019-03-05 21:57:24 +0800

    博客3.5

--- mypro/mysite/mysite/views.py
+++ mypro/mysite/mysite/views.py
@@ -1,3 +1,7 @@
 from django.http import HttpResponse
-def index(requset):
-    return HttpResponse('hello')
+from django.shortcuts import render
+# def index(requset):
+#     return HttpResponse('hello')
+
+def index(req):
+    return render(req,'index.html',{'msg':'这是首页'})
